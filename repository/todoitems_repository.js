7a5da8355412495a8b7433f6bf6371cbaaa0966c
ume_bar <kagari0915@gmail.com>
2021-09-28 16:13:26 +0900

    add Sequelize

--- repository/todoitems_repository.js
+++ repository/todoitems_repository.js
@@ -1,1 +1,12 @@
-// Postgresデータベースへのアクセスレイヤーとして機能
+// Postgresデータベースへのアクセスレイヤーとして機能 Sequelize
+var sequelize = new Sequelize(config.database, config.user, config.password, {
+    host: config.server,
+    dialect: 'postgres',
+    define: {
+        freezeTableName: true,
+        timestamps: true
+    }
+});
+
+// Sequelizeがコード内のモデルに名前を付ける方法をより細かく制御 freezeTabName
+// Postgresのを使用していることSequelizeを伝える dialect
